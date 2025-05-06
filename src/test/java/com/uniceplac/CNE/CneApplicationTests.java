335e225839ca2033a7a7aa7ab3cf902f54ce8e70
ed <eduardodsxavier@gmail.com>
2025-05-06 16:54:46 -0300

    setup of the enviroment

---
+++ src/test/java/com/uniceplac/CNE/CneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uniceplac.CNE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
