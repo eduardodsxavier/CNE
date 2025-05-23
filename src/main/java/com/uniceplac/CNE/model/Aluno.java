16b8b159f8ac7a182829097a3786b87ae7551724
marcelo914 <marcelosouza222244@gmail.com>
2025-05-23 12:54:20 -0300

    Criar aluno e disciplina, buscar pelo nome do aluno, responsavel e disciplina

--- src/main/java/com/uniceplac/CNE/model/Aluno.java
+++ src/main/java/com/uniceplac/CNE/model/Aluno.java
@@ -1,5 +1,6 @@
 package com.uniceplac.CNE.model;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -12,6 +13,8 @@ public class Aluno {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(nullable = false, unique = true)
+    private String ra;
     private String nome;
     private String email;
     private String turma;
@@ -20,8 +23,9 @@ public class Aluno {
     
     Aluno() {}
 
-    Aluno(long id, String nome, String email, String turma, String curso, boolean deleted) {
+    Aluno(long id, String ra, String nome, String email, String turma, String curso, boolean deleted) {
         this.id = id;
+        this.ra = ra;
         this.nome = nome;
         this.email = email;
         this.turma = turma;
@@ -33,6 +37,12 @@ public class Aluno {
         this.id = id;
     }
 
+    public String getRa() {
+        return ra;
+    }
+    public void setRa(String ra) {
+        this.ra = ra;
+    }
     public Long getId() {
         return id;
     }
--- src/main/java/com/uniceplac/CNE/repository/AlunoRepository.java
+++ src/main/java/com/uniceplac/CNE/repository/AlunoRepository.java
@@ -1,13 +1,18 @@
 package com.uniceplac.CNE.repository;
 
-import java.util.Optional;
-
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import java.util.List;
+import java.util.Optional;
 
 import com.uniceplac.CNE.model.Aluno;
 
 @Repository
-public interface AlunoRepository extends JpaRepository<Aluno, Long> {
-    Optional<Aluno> findById(Long id);
+public interface AlunoRepository extends JpaRepository<Aluno, String> {
+    @Query(value = "SELECT * FROM Aluno WHERE unaccent(nome) ILIKE unaccent(CONCAT('%', :nome, '%'))", nativeQuery = true)
+    List<Aluno> searchNameIgnoringAccent(@Param("nome") String nome);
+
+    Optional<Aluno> findByRa(String ra);
 }
