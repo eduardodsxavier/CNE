e2b9375a6bf9fd025780e147c8400102c3f814ad
unknown <marcosandreibci@gmail.com>
2025-06-04 15:30:15 -0300

    Interface_cenarios

--- src/main/java/com/uniceplac/CNE/dtos/DisciplinaDTO.java
+++
@@ -1,8 +0,0 @@
-package com.uniceplac.CNE.dtos;
-
-public class DisciplinaDTO {
-    public String nome;
-    public int cargaHoraria;
-    public String responsavelNome;
-    public String responsavelEmail;
-}
