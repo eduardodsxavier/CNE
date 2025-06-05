07bf0c7a111145c3a3997a06f345839162a0b54a
Marcelo <106995250+Marcelo914@users.noreply.github.com>
2025-06-05 08:54:37 -0300

    Add files via upload

---
+++ src/main/java/com/uniceplac/CNE/dtos/DisciplinaDTO.java
@@ -0,0 +1,8 @@
+package com.uniceplac.CNE.dtos;
+
+public class DisciplinaDTO {
+    public String nome;
+    public int cargaHoraria;
+    public String responsavelNome;
+    public String responsavelEmail;
+}
