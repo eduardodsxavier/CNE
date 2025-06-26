3fe67b002e712227f44d640fbc386c6be0b3e18a
DanielCs1609 <131261876+DanielCs1609@users.noreply.github.com>
2025-06-25 23:05:40 -0300

    Correção telas

--- src/main/java/com/uniceplac/CNE/controller/rotas/CalendarController.java
+++ src/main/java/com/uniceplac/CNE/controller/rotas/CalendarController.java
@@ -1,12 +1,12 @@
-package com.uniceplac.CNE.controller;
+package com.uniceplac.CNE.controller.rotas;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class CalendarController {
-    @GetMapping("/calendar")
-    private String calendar() {
-        return "calendario"; 
+    @GetMapping("/calendario")
+    private String calendario() {
+        return "interfaceCalendario"; 
     }
 }
