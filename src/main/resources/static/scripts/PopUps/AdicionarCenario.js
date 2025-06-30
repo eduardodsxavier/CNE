f712c683c7bb7b4401c337f7718caf98b6803d6c
DanielCs1609 <131261876+DanielCs1609@users.noreply.github.com>
2025-06-30 17:58:35 -0300

    VISUAL CADASTRO CENÁRIOS

--- src/main/resources/static/scripts/PopUps/AdicionarCenario.js
+++ src/main/resources/static/scripts/PopUps/AdicionarCenario.js
@@ -5,19 +5,24 @@ const steps = [
     title: 'Aluno',
     content: `
       <div class="form-group">
-        <label class="label">RA:*<input class="input" name="ra" placeholder="ra" required /></label>
+        <label for="aluno-ra" class="label">RA:<span class="required-asterisk">*</span></label>
+        <input id="aluno-ra" class="input" name="ra" placeholder="Digite o RA" required />
       </div>
       <div class="form-group">
-        <label class="label">Nome:*<input class="input" name="nome" placeholder="nome" required /></label>
+        <label for="aluno-nome" class="label">Nome:<span class="required-asterisk">*</span></label>
+        <input id="aluno-nome" class="input" name="nome" placeholder="Digite o nome completo" required />
       </div>
       <div class="form-group">
-        <label class="label">E-mail:*<input class="input" name="email" type="email" placeholder="e-mail" required /></label>
+        <label for="aluno-email" class="label">E-mail:<span class="required-asterisk">*</span></label>
+        <input id="aluno-email" class="input" name="email" type="email" placeholder="Digite o e-mail" required />
       </div>
       <div class="form-group">
-        <label class="label">Curso:*<input class="input" name="curso" placeholder="curso" required /></label>
+        <label for="aluno-curso" class="label">Curso:<span class="required-asterisk">*</span></label>
+        <input id="aluno-curso" class="input" name="curso" placeholder="Digite o curso" required />
       </div>
       <div class="form-group">
-        <label class="label">Turma:*<input class="input" name="turma" placeholder="turma" required /></label>
+        <label for="aluno-turma" class="label">Turma:<span class="required-asterisk">*</span></label>
+        <input id="aluno-turma" class="input" name="turma" placeholder="Digite a turma" required />
       </div>
     `
   },
@@ -25,16 +30,20 @@ const steps = [
     title: 'Disciplina',
     content: `
       <div class="form-group">
-        <label class="label">Nome:*<input class="input" name="nomeDisciplina" placeholder="nome da disciplina" required /></label>
+        <label for="disciplina-nome" class="label">Nome:<span class="required-asterisk">*</span></label>
+        <input id="disciplina-nome" class="input" name="nomeDisciplina" placeholder="Ex: Engenharia de Software" required />
       </div>
       <div class="form-group">
-        <label class="label">Carga Horária:*<input class="input" name="cargaHoraria" type="number" placeholder="carga horária" required /></label>
+        <label for="disciplina-carga" class="label">Carga Horária:<span class="required-asterisk">*</span></label>
+        <input id="disciplina-carga" class="input" name="cargaHoraria" type="number" placeholder="Ex: 80" required />
       </div>
       <div class="form-group">
-        <label class="label">Professor Orientador:*<input class="input" name="responsavelNome" placeholder="professor orientador" required /></label>
+        <label for="disciplina-orientador" class="label">Professor Orientador:<span class="required-asterisk">*</span></label>
+        <input id="disciplina-orientador" class="input" name="responsavelNome" placeholder="Digite o nome do professor" required />
       </div>
       <div class="form-group">
-        <label class="label">E-mail Orientador:*<input class="input" name="responsavelEmail" type="email" placeholder="e-mail orientador" required /></label>
+        <label for="disciplina-email-orientador" class="label">E-mail Orientador:<span class="required-asterisk">*</span></label>
+        <input id="disciplina-email-orientador" class="input" name="responsavelEmail" type="email" placeholder="Digite o e-mail do professor" required />
       </div>
     `
   },
@@ -42,109 +51,123 @@ const steps = [
     title: 'Unidade',
     content: `
       <div class="form-group">
-        <label class="label">Nome:*<input class="input" name="nomeUnidade" placeholder="nome da unidade" required /></label>
+        <label for="unidade-nome" class="label">Nome:<span class="required-asterisk">*</span></label>
+        <input id="unidade-nome" class="input" name="nomeUnidade" placeholder="Digite o nome da unidade" required />
       </div>
       <div class="form-group">
-        <label class="label">Sigla do Órgão:*<input class="input" name="sigla" placeholder="sigla" required /></label>
+        <label for="unidade-sigla" class="label">Sigla do Órgão:<span class="required-asterisk">*</span></label>
+        <input id="unidade-sigla" class="input" name="sigla" placeholder="Digite a sigla" required />
       </div>
-      <div class="form-group">
-        <span class="label">Interno:*
-          <label class="switch">
-            <input type="checkbox" name="interno" />
-            <span class="slider"></span>
-          </label>
-        </span>
+      <div class="form-group form-group-inline">
+        <label class="label">Interno:<span class="required-asterisk">*</span></label>
+        <label class="switch">
+          <input type="checkbox" name="interno" />
+          <span class="slider"></span>
+        </label>
       </div>
-      <div class="form-group">
-        <span class="label">Convênio Público:*
-          <label class="switch">
-            <input type="checkbox" name="convenioPublico" />
-            <span class="slider"></span>
-          </label>
-        </span>
+      <div class="form-group form-group-inline">
+        <label class="label">Convênio Público:<span class="required-asterisk">*</span></label>
+        <label class="switch">
+          <input type="checkbox" name="convenioPublico" />
+          <span class="slider"></span>
+        </label>
       </div>
     `
   },
   {
-  title: 'VLR',
-  content: `
-    <div class="form-group">
-      <label class="label">Preceptor:<input class="input" name="preceptor" type="number" step="0.01" placeholder="R$" required /></label>
-    </div>
-    <div class="form-group">
-      <label class="label">Gerenciamento:<input class="input" name="gerenciamento" type="number" step="0.01" placeholder="R$" required /></label>
-    </div>
-    <div class="form-group">
-      <label class="label">Total:<input class="input" name="total" type="number" step="0.01" placeholder="R$" required /></label>
-    </div>
-    <div class="form-group">
-      <label class="label">Total do Aluno:<input class="input" name="totalAluno" type="number" step="0.01" placeholder="R$" required /></label>
-    </div>
-  `
+    title: 'VLR',
+    content: `
+      <div class="form-group">
+        <label for="vlr-preceptor" class="label">Preceptor:</label>
+        <input id="vlr-preceptor" class="input" name="preceptor" type="number" step="0.01" placeholder="R$" required />
+      </div>
+      <div class="form-group">
+        <label for="vlr-gerenciamento" class="label">Gerenciamento:</label>
+        <input id="vlr-gerenciamento" class="input" name="gerenciamento" type="number" step="0.01" placeholder="R$" required />
+      </div>
+      <div class="form-group">
+        <label for="vlr-total" class="label">Total:</label>
+        <input id="vlr-total" class="input" name="total" type="number" step="0.01" placeholder="R$" required />
+      </div>
+      <div class="form-group">
+        <label for="vlr-total-aluno" class="label">Total do Aluno:</label>
+        <input id="vlr-total-aluno" class="input" name="totalAluno" type="number" step="0.01" placeholder="R$" required />
+      </div>
+    `
   },
-    {
-  title: 'TCE',
-  content: `
-    <div class="form-group">
-      <label class="label">Nome do Responsável:*<input class="input" name="nome" placeholder="nome do responsável" required /></label>
-    </div>
-    <div class="form-group">
-      <label class="label">Cargo:*<input class="input" name="cargo" placeholder="cargo" required /></label>
-    </div>
-    <div class="form-group">
-      <label class="label">E-mail:*<input class="input" name="email" type="email" placeholder="e-mail" required /></label>
-    </div>
-    <div class="form-group">
-      <label class="label">Telefone:*<input class="input" name="telefone" placeholder="telefone" required /></label>
-    </div>
-  `
-    },
-    {
+  {
+    title: 'TCE',
+    content: `
+      <div class="form-group">
+        <label for="tce-nome" class="label">Nome do Responsável:<span class="required-asterisk">*</span></label>
+        <input id="tce-nome" class="input" name="nome" placeholder="Digite o nome do responsável" required />
+      </div>
+      <div class="form-group">
+        <label for="tce-cargo" class="label">Cargo:<span class="required-asterisk">*</span></label>
+        <input id="tce-cargo" class="input" name="cargo" placeholder="Digite o cargo" required />
+      </div>
+      <div class="form-group">
+        <label for="tce-email" class="label">E-mail:<span class="required-asterisk">*</span></label>
+        <input id="tce-email" class="input" name="email" type="email" placeholder="Digite o e-mail" required />
+      </div>
+      <div class="form-group">
+        <label for="tce-telefone" class="label">Telefone:<span class="required-asterisk">*</span></label>
+        <input id="tce-telefone" class="input" name="telefone" placeholder="Digite o telefone" required />
+      </div>
+    `
+  },
+  {
     title: 'Data',
     content: `
-    <div class="form-group">
-      <label class="label">Início do Estágio:*<input class="input" name="inicioEstagio" type="date" required /></label>
-    </div>
-    <div class="form-group">
-      <label class="label">Término do Estágio:*<input class="input" name="terminoEstagio" type="date" required /></label>
-    </div>
-    <div class="form-group">
-      <label class="label">Dias da Semana:*<input class="input" name="diasSemana" placeholder="ex: seg, ter, qua" required /></label>
-    </div>
-    <div class="form-group">
-      <label class="label">
-        Feriado:<input type="checkbox" name="feriado" />
-      </label>
-    </div>
-`
-},
-    {
-  title: 'Horário',
-  content: `
-    <div class="form-group">
-      <label class="label">Horário Inicial:*<input class="input" name="horarioInicial" type="time" required /></label>
-    </div>
-    <div class="form-group">
-      <label class="label">Horário Final:*<input class="input" name="horarioFinal" type="time" required /></label>
-    </div>
-    <div class="form-group">
-      <label class="label">Quantidade de Horas:*<input class="input" name="qtdHoras" type="time" required /></label>
-    </div>
-    <div class="form-group">
-      <label class="label">Carga Diária:*<input class="input" name="cargaHoraria" type="number" required /></label>
-    </div>
-    <div class="form-group">
-      <label class="label">Turno:
-        <select class="input" name="turno" required>
+      <div class="form-group">
+        <label for="data-inicio" class="label">Início do Estágio:<span class="required-asterisk">*</span></label>
+        <input id="data-inicio" class="input" name="inicioEstagio" type="date" required />
+      </div>
+      <div class="form-group">
+        <label for="data-termino" class="label">Término do Estágio:<span class="required-asterisk">*</span></label>
+        <input id="data-termino" class="input" name="terminoEstagio" type="date" required />
+      </div>
+      <div class="form-group">
+        <label for="data-dias" class="label">Dias da Semana:<span class="required-asterisk">*</span></label>
+        <input id="data-dias" class="input" name="diasSemana" placeholder="Ex: seg, ter, qua" required />
+      </div>
+      <div class="form-group form-group-inline">
+        <input id="data-feriado" type="checkbox" name="feriado" />
+        <label for="data-feriado" class="label">Feriado</label>
+      </div>
+    `
+  },
+  {
+    title: 'Horário',
+    content: `
+      <div class="form-group">
+        <label for="horario-inicial" class="label">Horário Inicial:<span class="required-asterisk">*</span></label>
+        <input id="horario-inicial" class="input" name="horarioInicial" type="time" required />
+      </div>
+      <div class="form-group">
+        <label for="horario-final" class="label">Horário Final:<span class="required-asterisk">*</span></label>
+        <input id="horario-final" class="input" name="horarioFinal" type="time" required />
+      </div>
+      <div class="form-group">
+        <label for="horario-qtd" class="label">Quantidade de Horas:<span class="required-asterisk">*</span></label>
+        <input id="horario-qtd" class="input" name="qtdHoras" type="time" required />
+      </div>
+      <div class="form-group">
+        <label for="horario-carga-diaria" class="label">Carga Diária:<span class="required-asterisk">*</span></label>
+        <input id="horario-carga-diaria" class="input" name="cargaHoraria" type="number" required />
+      </div>
+      <div class="form-group">
+        <label for="horario-turno" class="label">Turno:<span class="required-asterisk">*</span></label>
+        <select id="horario-turno" class="input" name="turno" required>
+          <option value="" disabled selected>Selecione um turno</option>
           <option value="MATUTINO">Matutino</option>
           <option value="VESPERTINO">Vespertino</option>
           <option value="NOTURNO">Noturno</option>
           <option value="DIURNO">Diurno</option>
         </select>
-      </label>
-    </div>
-`
-    }
+      </div>
+    `
+  }
 ];
 
 const wizardData = {};
@@ -158,18 +181,20 @@ function showPopup() {
       <div class="modal-overlay" role="dialog" aria-modal="true">
         <div class="modal" aria-labelledby="title">
           <div class="modal-header">
-            ${step.title}
             <div class="steps">
               ${steps.map((_, i) => `<div class="step${i <= current ? ' active' : ''}"></div>`).join('')}
             </div>
+            <span class="modal-title" id="title">
+            ${step.title}
+            </span>
           </div>
           <div class="modal-content">${step.content}</div>
           <div class="modal-footer">
-            <button id="back" class="btn back" ${current === 0 ? 'disabled' : ''}>
-              <span class="icon">←</span> VOLTAR
+            <button id="back" class="btn btn-cancel" ${current === 0 ? 'disabled' : ''}>
+              <span class="icon"><i class="fa-solid fa-arrow-left"></i></span> VOLTAR
             </button>
-            <button id="next" class="btn save">
-              <span class="icon">${current === steps.length - 1 ? '✔' : '→'}</span> ${current === steps.length - 1 ? 'CONCLUIR' : 'AVANÇAR'}
+            <button id="next" class="btn btn-next">
+              <span class="icon">${current === steps.length - 1 ? '<i class="fa-solid fa-check"></i>'  : '<i class="fa-solid fa-arrow-right"></i>'}</span> ${current === steps.length - 1 ? 'CONCLUIR' : 'AVANÇAR'}
             </button>
           </div>
         </div>
