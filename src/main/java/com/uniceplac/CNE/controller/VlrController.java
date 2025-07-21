3626547add55cb827ab56d24f3944bb96b6e282f
ed <eduardodsxavier@gmail.com>
2025-07-21 19:18:44 -0300

    feat: endpoint that return Vlr

--- src/main/java/com/uniceplac/CNE/controller/VlrController.java
+++ src/main/java/com/uniceplac/CNE/controller/VlrController.java
@@ -2,7 +2,9 @@ package com.uniceplac.CNE.controller;
 
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.http.ResponseEntity;
 import com.uniceplac.CNE.repository.VlrRepository;
@@ -16,6 +18,12 @@ public class VlrController {
 
     public VlrController(VlrRepository vlrRepository) {
         this.vlrRepository = vlrRepository;
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Vlr> getVlr(@PathVariable Long id) {
+        Vlr valor = vlrRepository.findById(id).get();
+        return ResponseEntity.ok(valor);
     }
 
     @PostMapping
