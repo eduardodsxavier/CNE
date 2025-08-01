92fe5726b3d70b2a5f698f2add2ebca39b89cd4b
marcelo914 <marcelosouza222244@gmail.com>
2025-08-01 00:37:13 -0300

    efetuar login no enter

--- src/main/resources/static/scripts/login.js
+++ src/main/resources/static/scripts/login.js
@@ -1,6 +1,16 @@
 document.addEventListener("DOMContentLoaded", function () {
     const loginButton = document.querySelector(".btn-entrar");
 
+    // Permite login ao pressionar Enter em qualquer campo de input
+    document.querySelectorAll('.input-email, .input-senha').forEach(input => {
+        input.addEventListener('keydown', function(event) {
+            if (event.key === 'Enter') {
+                event.preventDefault();
+                loginButton.click();
+            }
+        });
+    });
+
     loginButton.addEventListener("click", async function (event) {
         event.preventDefault();
 
